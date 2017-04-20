e36c62249e7f0427b28f59a4ee893cfb4b2f6116
jorcalle11 <jorcalle11@gmail.com>
2017-04-20 17:46:07 -0500

    database

---
+++ database/db.js
@@ -0,0 +1,13 @@
+const states = require('../entities/states');
+const licenses = require('../entities/licenses');
+const employees = require('../entities/employees');
+const professions = require('../entities/professions');
+const workgroups = require('../entities/workgroups');
+
+module.exports = {
+  employees,
+  states,
+  licenses,
+  professions,
+  workgroups
+}
