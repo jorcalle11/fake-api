45f9e657e0d7340a6a6fce9d222b87d66b09de49
jorcalle11 <jorcalle11@gmail.com>
2017-05-12 13:34:55 -0500

    Add profession format field

--- entities/professions.js
+++ entities/professions.js
@@ -6,7 +6,11 @@ casual.define('entity_profession', id => {
     id,
     name: casual.title,
     stateId: casual.integer(from = 0, to = 19),
-    code: casual.state_abbr
+    code: casual.state_abbr,
+    boardId: casual.integer(from = 0, to = 19),
+    format: id % 2 ?
+      casual.populate('{{letter}}{{letter}}{{integer}}') :
+      casual.populate('{{letter}}{{integer}}{{letter}}')
   };
 });
 
